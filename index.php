66ad99d5a4610d1a9e8d5e6be96a1265d80bd634
JedBorseth <jedborseth@gmail.com>
2022-08-12 09:22:26 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+include_once("./includes/dbinfo.php");
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Jed's Database App</title>
+</head>
+<body>
+<h1 class="text-center text-4xl">Jed Borseth SQL App</h1>
+</body>
+</html>
